09efcdbbb2234e03b431c4c32d033690669c0340
Garry McGlennon <garrm@microsoft.com>
2015-04-17 16:13:39 -0700

    Adding missing files

---
+++ xpf.Http/BearerToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace xpf.Http
+{
+    public class BearerToken
+    {
+        public string Token { get; set; }
+
+        public string Type { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Expires { get; set; }
+
+        public bool HasExpired  {get { return this.Expires < DateTime.Now; }}
+    }
+}
